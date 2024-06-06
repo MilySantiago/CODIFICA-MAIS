e4bcaf3897a56ee72e92b03523e96c424045a0a5
unknown <Kamily Santiago>
2024-06-06 19:38:19 -0300

    exercicios-enviar para nova branch

---
+++ PHP-ARRAYS/exercicio-lista-array/exercicio-15.php
@@ -0,0 +1,22 @@
+<?php
+echo "Encontrar Elemento e Índice" .PHP_EOL;
+    $listaNumerica=array();
+    $verificado = false;
+
+    for ($i = 0; $i <= 6; $i++){
+        $listaNumerica[$i] =readline(" Este é o número " . ($i + 1) . ": ");
+}
+       
+    
+    echo "Escreva um número:";
+    $numero=trim(fgets(STDIN));
+
+    for ($i = 0; $i < 7; $i++){
+        if($listaNumerica[$i] == $numero){
+            echo "Este $numero está na lista, e a posição é $i." . PHP_EOL;
+            $verificado = true;
+        }
+    }
+    if ($verificado != true){
+        echo "Este número $numero não está  na lista.";
+    }
